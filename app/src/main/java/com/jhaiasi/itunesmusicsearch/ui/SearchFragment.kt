ac9d80b70b545b92fdf162a3c55b1f77d25f8c7b
Isaiah <ijbcatimpo@gmail.com>
2021-03-09 20:30:27 +0800

    Start adding UI elements and data binding

--- app/src/main/java/com/jhaiasi/itunesmusicsearch/ui/SearchFragment.kt
+++ app/src/main/java/com/jhaiasi/itunesmusicsearch/ui/SearchFragment.kt
@@ -1,4 +1,25 @@
 package com.jhaiasi.itunesmusicsearch.ui
 
-class SearchFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jhaiasi.itunesmusicsearch.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
 }
